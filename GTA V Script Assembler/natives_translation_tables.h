654be1597415a53003fadb1ded138d2afe2a2dca
Sasuke78200 <Sasuke782000@gmail.com>
2017-01-16 12:18:37 +0100

    Added the support for b944 natives.

--- GTA V Script Assembler/natives_translation_tables.h
+++ GTA V Script Assembler/natives_translation_tables.h
@@ -1,6 +1,6 @@
 #ifndef __NATIVES_TRANSLATION_TABLES_H__
 #define __NATIVES_TRANSLATION_TABLES_H__
 
-unsigned __int64 g_ullNativesHashTranslationTable[][5655][2];
+unsigned __int64 g_ullNativesHashTranslationTable[][5712][2];
 
 #endif
